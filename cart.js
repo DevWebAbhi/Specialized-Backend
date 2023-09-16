d09ccaf17dd45c123c4f310bd7e5ab8fae1f77e1
DevWebAbhi <tiwariabhishek889912@gmail.com>
2023-09-16 17:44:01 +0530

    Backend

--- cart.js
+++ cart.js
@@ -39,7 +39,7 @@ routerCart.post("/post",async(req,res)=>{
 routerCart.delete("/remove/:id",async(req,res)=>{
     const{id}=req.params;
     try {
-        await CartModel.delete({id:id});
+        await CartModel.deleteOne({id:id});
         res.send({msg:"deleted"});
     } catch (error) {
         res.send({msg:"error"});
