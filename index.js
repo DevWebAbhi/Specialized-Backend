a27535462bacac682095144fc85c4330e0a882b4
DevWebAbhi <tiwariabhishek889912@gmail.com>
2023-10-30 15:43:31 +0530

    Backend

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/",(req,res)=>{
     res.send({msg:"Welcome To Specliazed Backend"});
 })
 
+app.use('/Files', express.static(__dirname + '/uploads'));
 async function auth(req,res,next){
     const token=req.headers.authorization.split(" ")[1];
     console.log(JWTKEY,token)
